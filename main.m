904d9775cc2dc012dd4a749575a8b344f90b3ac8
Raquel <raquel2terrero@gmail.com>
2018-05-22 20:28:28 +0200

    m

--- main.m
+++ main.m
@@ -1,5 +1,8 @@
 im = read_lumfile('Still_images/camman.lum'); %IMAGEN CUADRADA
-N = 8; %TAMAÑO BLOQUES
+im = im/255; %normalizamos
+N = 4; %TAMAÑO BLOQUES
+%Nc = %COEFICIENTES A ENVIAR
+alpha = 0.5; %UMBRAL DE DECISIÓN
 %añadirmos zeros si hace falta
 sobran1 = mod(size(im,1),N);
 if sobran1
@@ -12,4 +15,11 @@ end
 %para cada bloque de NxN...
 for k=0:N:(length(im)-1)
     bloque = im(1+k:k+N,1+k:k+N);
+    desv = std(bloque(:));
+    if (desv<alpha)
+        B = dct2(bloque);
+        coefs = zigzag(B,Nc);
+    else
+        [Ur, X, Ul] = svd(bloque);
+    end    
 end
