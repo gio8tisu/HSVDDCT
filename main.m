e95306e2fdd489ce37a253c0a2f77a4b60017295
Sergio <gio8.tisu@gmail.com>
2018-05-22 18:54:09 +0200

    main

--- main.m
+++ main.m
@@ -1,4 +1,15 @@
-im = read_lumfile('camman.lum');
-%%Dividimos una imagen en bloques
-N = 8;
+im = read_lumfile('camman.lum'); %IMAGEN CUADRADA
+N = 8; %TAMAÑO BLOQUES
+%añadirmos zeros si hace falta
+sobran1 = mod(size(im,1),N);
+if sobran1
+    sobran2 = mod(size(im,2),N);
+    if sobran2
+        %TODO
+    end
+end
 
+%para cada bloque de NxN...
+for k=1:length(im)/N
+    bloque = im(k,k+N);
+end
