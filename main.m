002777631d37e318b5beda4fb224024498cdeb4c
Raquel <raquel2terrero@gmail.com>
2018-05-22 19:02:32 +0200

    bloque correcto

--- main.m
+++ main.m
@@ -1,4 +1,4 @@
-im = read_lumfile('camman.lum'); %IMAGEN CUADRADA
+im = read_lumfile('Still_images/camman.lum'); %IMAGEN CUADRADA
 N = 8; %TAMAÑO BLOQUES
 %añadirmos zeros si hace falta
 sobran1 = mod(size(im,1),N);
@@ -10,6 +10,6 @@ if sobran1
 end
 
 %para cada bloque de NxN...
-for k=1:length(im)/N
-    bloque = im(k,k+N);
+for k=0:N:(length(im)-1)
+    bloque = im(1+k:k+N,1+k:k+N);
 end
