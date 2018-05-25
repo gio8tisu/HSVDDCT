f03eb2c0a6fc031b7ae67fe346f8fde090309d86
Sergio <gio8.tisu@gmail.com>
2018-05-26 01:25:13 +0200

    SVD

--- my_svd_inv.m
+++ my_svd_inv.m
@@ -1,5 +1,6 @@
 function [X_rec] = my_svd_inv(U_l, c, U_r)
-%INVERSE SVD TRANSFORM reconstruct matrix X from p eigenvectors
+%INVERSE SVD TRANSFORM reconstruct matrix X from p matrices and
+%singular values
 %   Detailed explanation goes here
     X_rec = zeros(size(U_l,1),size(U_r,1));
     for i=1:length(c)
