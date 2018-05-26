28a8b3af62a800e1cb67aa0a59a08b45c91863d9
Raquel <raquel2terrero@gmail.com>
2018-05-26 19:11:07 +0200

    zigzag & unzigzag

--- unzigzag.m
+++ unzigzag.m
@@ -1,7 +1,37 @@
-function [dct] = unzigzag(coeffs,N)
-%UNTITLED3 Summary of this function goes here
+function [B] = unzigzag(coeffs, N)
+%UNZIGZAG creates NxN matrix filled with coeffs in a zigzag order
 %   Detailed explanation goes here
-    outputArg1 = inputArg1;
-    outputArg2 = inputArg2;
+    B=zeros(N);
+    cursor = [1 1];
+    cont = 1;
+    coeffs = [coeffs;zeros(N*N-size(coeffs,1),1)]
+    while cont<=(N*N)
+        if cursor(2)==1 && cursor(1)<size(B,1) || cursor(2)==size(B,2)
+            B(cursor(1),cursor(2)) = coeffs(cont);
+            cursor(1) = cursor(1) + 1;
+            cont = cont + 1;
+        elseif cursor(1)==1 && cursor(2)<size(B,2) || cursor(1)==size(B,1)
+            B(cursor(1),cursor(2)) = coeffs(cont);
+            cursor(2) = cursor(2) + 1;
+            cont = cont + 1;
+        end
+        if cursor(2)==1 || cursor(1)==size(B,2)
+            while cursor(1)~=1 && cursor(2)~=size(B,2) && cont<=(N*N)
+                %subir en diagonal
+                B(cursor(1),cursor(2)) = coeffs(cont);
+                cursor(2) = cursor(2) + 1;
+                cursor(1) = cursor(1) - 1;
+                cont = cont + 1;
+            end
+        elseif cursor(1)==1 || cursor(2)==size(B,1)
+            while cursor(2)~=1 && cursor(1)~=size(B,1) && cont<=(N*N)
+                %bajar en diagonal
+                B(cursor(1),cursor(2)) = coeffs(cont);
+                cursor(1) = cursor(1) + 1;
+                cursor(2) = cursor(2) - 1;
+                cont = cont + 1;
+            end
+        end  
+    end
 end
 
--- zigzag.m
+++ zigzag.m
@@ -1,8 +1,38 @@
-function [coeffs] = zigzag(dct_c, n_c)
-%UNTITLED2 Summary of this function goes here
+function [coeffs] = zigzag(B, n_c)
+%ZIGZAG flattens matrix in a zigzag order
 %   Detailed explanation goes here
     coeffs = zeros(n_c,1);
     cursor = [1 1];
-    coeffs(1) = dct_c(cursor(1),cursor(2));
+    cont = 1;
+    while cont<=n_c
+        %bajar una posicion
+        if cursor(2)==1 && cursor(1)<size(B,1) || cursor(2)==size(B,2)
+            coeffs(cont) = B(cursor(1),cursor(2));
+            cursor(1) = cursor(1) + 1;
+            cont = cont + 1;
+        %mover derecha una posicion
+        elseif cursor(1)==1 && cursor(2)<size(B,2) || cursor(1)==size(B,1)
+            coeffs(cont) = B(cursor(1),cursor(2));
+            cursor(2) = cursor(2) + 1;
+            cont = cont + 1;
+        end
+        if cursor(2)==1 || cursor(1)==size(B,2)
+            while cursor(1)~=1 && cursor(2)~=size(B,2) && cont<=n_c
+                %subir en diagonal
+                coeffs(cont) = B(cursor(1),cursor(2));
+                cursor(2) = cursor(2) + 1;
+                cursor(1) = cursor(1) - 1;
+                cont = cont + 1;
+            end
+        elseif cursor(1)==1 || cursor(2)==size(B,1)
+            while cursor(2)~=1 && cursor(1)~=size(B,1) && cont<=n_c
+                %bajar en diagonal
+                coeffs(cont) = B(cursor(1),cursor(2));
+                cursor(1) = cursor(1) + 1;
+                cursor(2) = cursor(2) - 1;
+                cont = cont + 1;
+            end
+        end  
+    end
 end
 
