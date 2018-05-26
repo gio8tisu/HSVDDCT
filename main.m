7afbfe4bf911911dc805c7f6f23d2613e87175ba
Sergio <gio8.tisu@gmail.com>
2018-05-26 19:35:11 +0200

    main

--- main.m
+++ main.m
@@ -1,19 +1,24 @@
+%% Leer imagen y definir parametros
 im = read_lumfile('Still_images/camman.lum'); %IMAGEN CUADRADA
 im = im/255; %normalizamos
 N = 4; %TAMAÑO BLOQUES
-Nc = %COEFICIENTES A ENVIAR
-alpha = 0.5; %UMBRAL DE DECISIÓN
-beta = 0.5; %PARAMETRO PARA NUMERO DE AUTOVECTORES
+Nc = 12; %COEFICIENTES A ENVIAR
+alpha = 0.2; %UMBRAL DE DECISIÓN
+beta = 0.8; %PARAMETRO PARA NUMERO DE AUTOVECTORES
+
+%% Codificador
 %añadirmos zeros si hace falta
 sobran1 = mod(size(im,1),N);
 if sobran1
+    im = [im; zeros(N-sobran1, size(im,2))];
     sobran2 = mod(size(im,2),N);
     if sobran2
-        %TODO
+        im = [im, zeros(size(im,1),N-sobran2)];
     end
 end
 
 %para cada bloque de NxN...
+im_rec = [];
 for k=0:N:(length(im)-1)
     bloque = im(1+k:k+N,1+k:k+N);
     desv = std(bloque(:));
@@ -21,6 +26,8 @@ for k=0:N:(length(im)-1)
         B = dct2(bloque);
         coefs = zigzag(B,Nc);
     else
-        [Ur, X, Ul] = svd(bloque);
+        [U_r, c, U_l] = my_svd(bloque, beta);
+        bloque_rec = my_svd_inv(U_l, c, U_r);
+        im_rec = [im_rec; bloque_rec];
     end    
 end
